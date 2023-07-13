56cc8bcf645028df629d3dc94fb3ceeee611e264
vaibhav <vaibhav@gmail.com>
2023-07-13 15:15:03 +0530

    changes

--- vpc.tf
+++ vpc.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "public1" {
 resource "aws_subnet" "public2" {
   vpc_id     = aws_vpc.terraform.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-1b"
+  availability_zone = "us-west-1c"
 
   tags = {
     Name = "public2"
@@ -39,7 +39,7 @@ resource "aws_subnet" "private1" {
 resource "aws_subnet" "private2" {
   vpc_id     = aws_vpc.terraform.id
   cidr_block = "10.0.3.0/24"
-  availability_zone = "us-west-1b"
+  availability_zone = "us-west-1c"
 
   tags = {
     Name = "private2"
@@ -98,4 +98,38 @@ resource "aws_route_table_association" "rt2" {
 resource "aws_route_table_association" "rt3" {
   subnet_id      = aws_subnet.private2.id
   route_table_id = aws_route_table.private.id
+}
+
+resource "aws_nat_gateway" "nat-pub" {
+  allocation_id = aws_eip.eip-terraform.id
+  subnet_id     = aws_subnet.public1.id
+
+  tags = {
+    Name = "public-nat-gateway"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.terraform-igw]
+}
+
+resource "aws_eip" "eip-terraform" {
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "nat-pub1" {
+  allocation_id = aws_eip.eip-terraform1.id
+  subnet_id     = aws_subnet.public2.id
+
+  tags = {
+    Name = "public-nat-gateway"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.terraform-igw]
+}
+
+resource "aws_eip" "eip-terraform1" {
+  vpc      = true
 }
