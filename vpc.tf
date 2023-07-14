a7210989ee10259352249d93349ed9a056f6e51a
Mayur <mayurtannirwar0123@gmail.com>
2023-07-14 12:06:32 +0530

    update file

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,10 @@
+locals {
+  public_cidr = ["10.0.1.0/24","10.0.2.0/24"]
+  private_cidr = ["10.0.3.0/24","10.0.4.0/24"]
+  availability_zone = ["us-west-1a", "us-west-1c"]
+}
+
+
 resource "aws_vpc" "terraform" {
   cidr_block = "10.0.0.0/16"
 
@@ -6,34 +13,26 @@ resource "aws_vpc" "terraform" {
   }
 
 }
-resource "aws_subnet" "public1" {
+resource "aws_subnet" "public" {
   count = 2
 
   vpc_id     = aws_vpc.terraform.id
-  cidr_block = "10.0.${count.index}.0/24"
-  availability_zone = "us-west-1a"
+  cidr_block = local.public_cidr[count.index]
+  availability_zone = local.availability_zone[count.index]
   tags = {
-    Name = "public${count.index}"
+    Name = "public${count.index+1}"
   }
 }
 
-resource "aws_subnet" "private1" {
+resource "aws_subnet" "private" {
+  count = 2
+
   vpc_id     = aws_vpc.terraform.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-west-1a"
-
+  cidr_block =  local.private_cidr[count.index]
+  availability_zone = local.availability_zone[count.index]
   tags = {
-    Name = "private1"
+    Name = "private${count.index+1}"
   }
 
 }
 
-resource "aws_subnet" "private2" {
-  vpc_id     = aws_vpc.terraform.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = "us-west-1c"
-
-  tags = {
-    Name = "private2"
-  }
-}
