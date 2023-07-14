c6253cb14958bd40d67e4d929e41e616139f8a57
Mayur <mayurtannirwar0123@gmail.com>
2023-07-14 13:19:58 +0530

    automation code

--- vpc.tf
+++ vpc.tf
@@ -19,6 +19,7 @@ resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.terraform.id
   cidr_block = local.public_cidr[count.index]
   availability_zone = local.availability_zone[count.index]
+
   tags = {
     Name = "public${count.index+1}"
   }
@@ -30,9 +31,61 @@ resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.terraform.id
   cidr_block =  local.private_cidr[count.index]
   availability_zone = local.availability_zone[count.index]
+
   tags = {
     Name = "private${count.index+1}"
   }
 
 }
 
+resource "aws_internet_gateway" "terraform-igw" {
+  vpc_id = aws_vpc.terraform.id
+
+  tags = {
+    Name = "igw-terraform"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.terraform.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terraform-igw.id
+  }
+
+  tags = {
+    Name = "rt-terraform-pub"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.terraform.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terraform-igw.id
+  }
+
+  tags = {
+    Name = "rt-terraform-pub"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+
+resource "aws_eip" "eip-terraform" {
+  count = 2
+  vpc      = true
+}
