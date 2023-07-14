95e38bcd8f4e8e7bae87feae96f6f79d1821f4b8
Mayur <mayurtannirwar0123@gmail.com>
2023-07-14 17:48:17 +0530

    new changes

--- vpc.tf
+++ vpc.tf
@@ -114,4 +114,8 @@ resource "aws_nat_gateway" "nat-pub" {
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.terraform-igw]
-}
+
+ }
+
+
+ #this is my new sentence
